77878977417e19b568e3e1562080f8718eba9bc7
putrialiciawati <putrialiciawati@gmail.com>
2026-01-06 10:07:31 +0700

    Create persegi.py

---
+++ persegi.py
@@ -0,0 +1,15 @@
+def hitung_luas_persegi(sisi):
+    return sisi * sisi
+
+def hitung_keliling_persegi(sisi):
+    return 4 * sisi
+
+
+# Contoh penggunaan
+sisi = 5
+luas = hitung_luas_persegi(sisi)
+keliling = hitung_keliling_persegi(sisi)
+
+print("Sisi persegi:", sisi)
+print("Luas persegi:", luas)
+print("Keliling persegi:", keliling)
